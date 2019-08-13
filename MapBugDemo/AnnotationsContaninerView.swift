9e36195a242613096e2ac7138df2145afdb4e861
Gabriel Lanata <glanata@lyft.com>
2019-08-12 17:16:35 -0700

    Simplify

--- MapBugDemo/AnnotationsContaninerView.swift
+++ MapBugDemo/AnnotationsContaninerView.swift
@@ -1,22 +1,8 @@
 import UIKit
 import GoogleMaps
 
-class AnnotationsContainerView: UIView {
-    var annotations = [Annotation]()
-
-    func add(_ annotation: Annotation) {
-        self.annotations.append(annotation)
-        self.addSubview(annotation)
-    }
-
-    func update(for mapView: GMSMapView) {
-        let projection = mapView.projection
-        for annotation in self.annotations {
-            annotation.layer.position = projection.point(for: annotation.coordinate)
-        }
-    }
-
-    // This prevents UIKit from messing with views since they are laid out manually.
+class NoAutolayoutView: UIView {
+    // This prevents autolayout from messing with views since they are laid out manually.
     public override func layoutSubviews() {}
     public override func layoutSublayers(of layer: CALayer) {}
 }
--- MapBugDemo/ViewController.swift
+++ MapBugDemo/ViewController.swift
@@ -6,22 +6,26 @@ private let kCoordinate = CLLocationCoordinate2D(latitude: -33.86, longitude: 15
 class ViewController: UIViewController {
 
     @IBOutlet var mapView: GMSMapView!
+    @IBOutlet var noAutolayoutView: NoAutolayoutView!
 
-    @IBOutlet var annotationsContainerView: AnnotationsContainerView!
+    let projectionView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.annotationsContainerView.isUserInteractionEnabled = false
-
+        // Set up map
         self.mapView.delegate = self
-        self.mapView.camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        self.mapView.camera = GMSCameraPosition.camera(withTarget: kCoordinate, zoom: 6.0)
 
+        // Set up marker
         let marker = GMSMarker(position: kCoordinate)
         marker.map = self.mapView
 
-        let annotation = PointAnnotation(coordinate: kCoordinate)
-        self.annotationsContainerView.add(annotation)
+        // Set up projection view
+        self.projectionView.backgroundColor = .purple
+        self.projectionView.layer.cornerRadius = 5
+        self.noAutolayoutView.addSubview(self.projectionView)
+        self.noAutolayoutView.isUserInteractionEnabled = false
     }
 
     @IBAction func toggleNavigationBar(_ sender: Any) {
@@ -32,10 +36,10 @@ class ViewController: UIViewController {
 
 extension ViewController: GMSMapViewDelegate {
     public func mapView(_ mapView: GMSMapView, didChange position: GMSCameraPosition) {
-        self.annotationsContainerView.update(for: mapView)
+        self.projectionView.center = mapView.projection.point(for: kCoordinate)
     }
 
     public func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
-        self.annotationsContainerView.update(for: mapView)
+        self.projectionView.center = mapView.projection.point(for: kCoordinate)
     }
 }
